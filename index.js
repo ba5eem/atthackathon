388ef06f54ce293327a76659a22453c5c009e1da
ba5eem <baseem@gmail.com>
2018-03-10 10:52:35 -1000

    :smile:

--- index.js
+++ index.js
@@ -2,23 +2,30 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const axios = require('axios');
-const search = require('youtube-search');
-const API_KEY = process.env.VISION_KEY;
+const search = require('youtube-search');//npm package
 
 
-var opts = {
-  maxResults: 10,
-  key: 'yourkey'
-};
-
+const opts = {
+  maxResults: 1,
+  key: process.env.VISION_KEY // 
+}; //options
 
 app.get('/', (req, res) => {
-	// youtube api
-
 	res.json('ola');
+})
+
+
+app.get('/:id', (req, res) => {
+	let query = req.params.id;
+	search(query, opts, function(err, results) {
+	  if(err) return console.log(err);
+	  res.json(results[0].link);// output is link of search
+	});
 })
 
 
 
 
 app.listen(9000);
+
+//olena 
