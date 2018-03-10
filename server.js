e3359201174d7fc97156a82fe6476ff82f16d123
Ella Mae A <ellamae207@gmail.com>
2018-03-09 23:40:46 -1000

    added body-parser

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 8888;
 
 // Connect and configure AWS
 const AWS = require('aws-sdk');
 const AWS = require('aws-sdk/global');
-const S3 = require('aws-sdk/clients/s3');
 AWS.config.update({region: 'us-east-1'});
 
-// Uploads the image to an S3 bucket
-
-const params = {
-  Image: {
-    S3Object: {
-      Bucket: 'cs591-mean',
-      Name: 'utest.jpg'
-    }
-  }
-};
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const rekognition = new AWS.Rekognition();
 
@@ -26,11 +19,14 @@ router.get('/', function(req, res, next) {
     if (err) console.log(err, err.stack);
       else {
       name = data.screenshotFace[0].Name;
-      id = data.screenshotFace[0].Urls[0].match(/nm(.*)/)[0]; /
+      id = data.screenshotFace[0].Urls[0].match(/nm(.*)/)[0];
       res.send(name)
     }
   });
 });
 
 
-module.exports = router;
+app.listen(PORT, () => {
+  db.sequelize.sync({ force: false });
+  console.log(`Listening on port: ${PORT}`);
+});
