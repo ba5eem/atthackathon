d2c301679cec0499a1666f5985e3f714e47caaa4
Ella Mae A <ellamae207@gmail.com>
2018-03-09 22:41:24 -1000

    renames

--- server.js
+++ server.js
@@ -22,15 +22,15 @@ const params = {
 const rekognition = new AWS.Rekognition();
 
 router.get('/', function(req, res, next) {
-  rekognition.recognizeCelebrities(params, function (err, data) {
+  rekognition.recognizeFace(params, function (err, data) {
     if (err) console.log(err, err.stack);
-    else {
-      name = data.CelebrityFaces[0].Name; // Pulls celebrity name from response
-      id = data.CelebrityFaces[0].Urls[0].match(/nm(.*)/)[0]; // Pulls IMDB ID using RegEx
+      else {
+      name = data.screenshotFace[0].Name;
+      id = data.screenshotFace[0].Urls[0].match(/nm(.*)/)[0]; /
       res.send(name)
-
     }
   });
 });
+
 
 module.exports = router;
