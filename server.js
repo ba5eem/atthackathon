f184d7a8d645c6d073fe17b4dfe68156f5bd5e11
Ella Mae A <ellamae207@gmail.com>
2018-03-10 16:06:40 -1000

    added path

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app             = express();
 const PORT            = process.env.PORT || 8080;
 const axios = require('axios');
 const vision = require('node-cloud-vision-api');
+const path            = require('path');
 vision.init({auth: process.env.VISION_KEY});
 
 var fs = require('fs');
